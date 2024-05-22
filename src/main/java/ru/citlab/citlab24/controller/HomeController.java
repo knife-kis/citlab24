23c9db10b13b1340c551546b80c48aa80c46211c
knife-kis <tarnovskiym@gmail.com>
2024-05-22 11:40:42 +0700

    Исправлены фрагменты, баг с подсвечиванием, переписан на таймлиф

--- src/main/java/ru/citlab/citlab24/controller/HomeController.java
+++ src/main/java/ru/citlab/citlab24/controller/HomeController.java
@@ -1,5 +1,7 @@
 package ru.citlab.citlab24.controller;
 
+
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,16 +10,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String home(Model model) {
+    public String home(HttpServletRequest request, Model model) {
+        model.addAttribute("currentPath", request.getRequestURI());
         return "index";
     }
 
     @GetMapping("/contact")
-    public String contact(Model model) {
+    public String contact(HttpServletRequest request, Model model) {
+        model.addAttribute("currentPath", request.getRequestURI());
         return "contact";
     }
     @GetMapping("/service-details")
-    public String service(Model model) {
+    public String service(HttpServletRequest request, Model model) {
+        model.addAttribute("currentPath", request.getRequestURI());
         return "service-details";
     }
 }
--- src/main/java/ru/citlab/citlab24/model/User.java
+++ src/main/java/ru/citlab/citlab24/model/User.java
@@ -1,18 +1,28 @@
 package ru.citlab.citlab24.model;
 
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 @Entity
 @Data
 public class User {
+    @jakarta.persistence.Id
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
